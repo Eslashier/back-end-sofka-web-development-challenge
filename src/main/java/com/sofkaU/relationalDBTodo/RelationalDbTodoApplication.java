ca91342523fca88d61f5e3b5291342a463b93555
Eslashier <anfelov@gmail.com>
2022-05-04 14:44:31 -0500

    git initialization

---
+++ src/main/java/com/sofkaU/relationalDBTodo/RelationalDbTodoApplication.java
@@ -0,0 +1,13 @@
+package com.sofkaU.relationalDBTodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationalDbTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationalDbTodoApplication.class, args);
+	}
+
+}
