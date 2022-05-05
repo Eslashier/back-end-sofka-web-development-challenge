e98875b76fcc7a4c3584a4c87cf64b246bc52039
Eslashier <anfelov@gmail.com>
2022-05-04 19:28:02 -0500

    entities created

--- src/main/java/com/sofkaU/relationalDBTodo/entity/ToDoList.java
+++ src/main/java/com/sofkaU/relationalDBTodo/entity/ToDoList.java
@@ -3,18 +3,32 @@ package com.sofkaU.relationalDBTodo.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity(name = "ToDoList")
 @Table(name = "toDoList")
 @Data
 public class ToDoList {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String listName;
+
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+
+    private List<ToDoTask> todoTasks = new ArrayList<>();
+
+    public ToDoList addToDoTask(ToDoTask toDoTask){
+        this.todoTasks.add(toDoTask);
+
+        return this;
+    }
 
 
 }
